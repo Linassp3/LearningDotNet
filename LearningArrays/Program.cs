77dc6d0e52491cde1f31b8e61eb03dbde63b9164
LinasP <linassp3@gmail.com>
2021-05-18 16:23:41 +0300

    Update

---
+++ LearningArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrays
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string[] zodziai = new string[5];
+            zodziai[0] = "kaciukas";
+            zodziai[1] = "suniukas";
+            zodziai[2] = "peliukas";
+            zodziai[3] = "triusiukas";
+            zodziai[4] = "drambliukas";
+            for (int i = 0; i < zodziai.Length; i++)
+                Console.WriteLine(zodziai[i]);
+            int[] skaiciukai = new int[20];
+            for (int i = 0; i < skaiciukai.Length; i++)
+                skaiciukai[i] = i;
+            for (int i = skaiciukai.Length - 1; i >= 0; i--)
+                Console.WriteLine(skaiciukai[i]);
+        }
+    }
+}
